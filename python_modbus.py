83567bd9645008a44abbc5adac37a8502ae5f24d
Vasja <voitenco@gmail.com>
2022-09-11 21:29:24 +0300

    Update python_modbus.py

--- python_modbus.py
+++ python_modbus.py
@@ -10,31 +10,33 @@ modbus2 = minimalmodbus.Instrument('/dev/ttyS2', 32)# port name, slave address
 
 
 while True:
-    time.sleep(2)
+    time.sleep(0.5)
 
     try:
         temp1 = modbus1.read_register(1, 0, 4, True)
         hum1 = modbus1.read_register(2, 0, 4, True)
     except IOError:
-        print('Error modbus -IOError- read to adress')
+        print('Error modbus1 -IOError-')
     except ValueError:
-        print('Error modbus -ValueError-  read to adress')
+        print('Error modbus1 -ValueError-')
     except TypeError:
-        print('Error modbus -TypeError-  read to adress')
+        print('Error modbus -TypeError-')
+    
+    print("==========31=============")    
+    print(temp1/10)
+    print(hum1/10) 
      
-    print(temp1)
-    print(hum1) 
-     
-    time.sleep(2)
+    time.sleep(0.5)
 
     try:
         temp2 = modbus2.read_register(1, 0, 4, True)
         hum2 = modbus2.read_register(2, 0, 4, True)
     except IOError:
-        print('Error modbus -IOError- read to adress')
+        print('Error modbus2 -IOError-')
     except ValueError:
-        print('Error modbus -ValueError-  read to adress')
+        print('Error modbus2 -ValueError-')
     except TypeError:
-        print('Error modbus -TypeError-  read to adress')
-    print(temp2)        
-    print(hum2)     
+        print('Error modbus2 -TypeError-')
+    print("==========32=============")    
+    print(temp2/10)        
+    print(hum2/10)   
